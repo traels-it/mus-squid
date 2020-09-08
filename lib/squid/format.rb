2c9906c2b98dca7426dc775107ceb149c0ea2291
Kasper Meyer <meyer@traels.it>
2020-09-08 10:44:59 +0200

    Add new label formats `:integer_without_zero` and `:float_without_zero`

--- lib/squid/format.rb
+++ lib/squid/format.rb
@@ -11,6 +11,8 @@ module Squid
         when :currency then number_to_currency value
         when :seconds then number_to_minutes_and_seconds value
         when :float then number_to_float value
+        when :integer_without_zero then number_to_integer_without_zero value
+        when :float_without_zero then number_to_float_without_zero value
         else number_to_delimited value.to_i
       end.to_s
     end
@@ -24,6 +26,14 @@ module Squid
     def number_to_float(value)
       float = number_to_rounded value, significant: true, precision: 2
       number_to_delimited float
+    end
+
+    def number_to_float_without_zero(value)
+      value.zero? ? "" : number_to_float(value)
+    end
+
+    def number_to_integer_without_zero(value)
+      value.zero? ? "" : number_to_delimited(value.to_i)
     end
   end
 end
