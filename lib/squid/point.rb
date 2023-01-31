848d56a8892b1619379d9fe6ede4708d3ae1fc92
Kasper Meyer <hi@kaspermeyer.com>
2023-01-31 06:55:28 +0100

    Feature. Ensure minimum height on bar charts (#1)

--- lib/squid/point.rb
+++ lib/squid/point.rb
@@ -15,18 +15,39 @@ module Squid
           y = y_for value, index: i, stack: stack, &offset if value
           y = y - offset.call([min, 0].min) if value
           label = format_for value, formats[series_i] if labels[series_i]
-          new y: y, height: h, index: i, label: label, negative: value.to_f < 0
+          new y: y, height: h, index: i, label: label, stack: stack, negative: value.to_f < 0
         end
       end
     end
 
-    attr_reader :y, :height, :index, :label, :negative
+    attr_reader :index, :label, :negative, :stack
 
-    def initialize(y:, height:, index:, label:, negative:)
-      @y, @height, @index, @label, @negative = y, height, index, label, negative
+    def initialize(y:, height:, index:, label:, negative:, stack:)
+      @y, @height, @index, @label, @negative, @stack = y, height, index, label, negative, stack
+    end
+
+    def height
+      return unless @height
+      @height + height_offset
+    end
+
+    def y
+      return unless @y
+      @y + height_offset
     end
 
   private
+
+    # Ensures values of zero are visible by setting a minimum height
+    #
+    # @note
+    #   We also adjust the height of zero-values on charts with negative values.
+    #   If this is deemed undesirable in the future, change the code below to:
+    #     @y == 0 && @height == 0 && !stack ? 2 : 0
+    #
+    def height_offset
+      @height == 0 && !stack ? 2 : 0
+    end
 
     def self.y_for(value, index:, stack:, &block)
       if stack
